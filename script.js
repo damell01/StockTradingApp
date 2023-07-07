b0db7481e429161ac036fa8979447032a365fec2
DamellBell <damellbell01@gmail.com>
2023-07-07 05:55:03 -0500

    Added ability to switch charts

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
 let aaplPriceData = [];
 let googlPriceData = [];
+let aaplSharesOwned = 0;
+let googlSharesOwned = 0;
 
-// Generate random price data
-function generatePriceData() {
+
+// Function to generate random stock prices
+function generateRandomPrices() {
   return Math.random() * 1000;
 }
 
@@ -13,7 +15,7 @@ function createChart(chartId, label, priceData) {
   return new Chart(ctx, {
     type: 'line',
     data: {
-      labels: Array.from({ length: 10 }, (_, i) => `Day ${i + 1}`),
+      labels: Array.from({ length: priceData.length }, (_, i) => `Day ${i + 1}`),
       datasets: [
         {
           label: label,
@@ -34,6 +36,9 @@ function createChart(chartId, label, priceData) {
             display: true,
             text: 'Time',
           },
+          ticks: {
+            maxTicksLimit: 10,
+          },
         },
         y: {
           display: true,
@@ -50,9 +55,72 @@ function createChart(chartId, label, priceData) {
   });
 }
 
+// Function to update stock prices every second
+function updateStockPrices() {
+  aaplPriceData.push(generateRandomPrices());
+  googlPriceData.push(generateRandomPrices());
+
+  // Keep the price data array limited to the last 10 prices
+  if (aaplPriceData.length > 10) {
+    aaplPriceData.shift();
+  }
+  if (googlPriceData.length > 10) {
+    googlPriceData.shift();
+  }
+
+  // Update the AAPL and GOOGL charts with the new data
+  aaplChart.data.datasets[0].data = aaplPriceData;
+  aaplChart.data.labels = Array.from({ length: aaplPriceData.length }, (_, i) => `Day ${i + 1}`);
+  aaplChart.options.scales.x.max = `Day ${aaplPriceData.length}`;
+  aaplChart.update();
+
+  googlChart.data.datasets[0].data = googlPriceData;
+  googlChart.data.labels = Array.from({ length: googlPriceData.length }, (_, i) => `Day ${i + 1}`);
+  googlChart.options.scales.x.max = `Day ${googlPriceData.length}`;
+  googlChart.update();
+}
+
+// Function to change chart interval
+function changeChartInterval(interval) {
+  let numDataPoints;
+  let dataInterval;
+  let maxTicksLimit;
+
+  if (interval === 'daily') {
+    numDataPoints = 10;
+    dataInterval = 'Day';
+    maxTicksLimit = 10;
+  } else if (interval === 'hourly') {
+    numDataPoints = 24;
+    dataInterval = 'Hour';
+    maxTicksLimit = 24;
+  } else if (interval === 'minute') {
+    numDataPoints = 60;
+    dataInterval = 'Minute';
+    maxTicksLimit = 60;
+  } else {
+    return; // Invalid interval
+  }
+
+  // Generate new data based on the selected interval
+  aaplPriceData = Array.from({ length: numDataPoints }, generateRandomPrices);
+  googlPriceData = Array.from({ length: numDataPoints }, generateRandomPrices);
+
+  // Update the chart data and options
+  aaplChart.data.datasets[0].data = aaplPriceData;
+  aaplChart.data.labels = Array.from({ length: numDataPoints }, (_, i) => `${dataInterval} ${i + 1}`);
+  aaplChart.options.scales.x.ticks.maxTicksLimit = maxTicksLimit;
+  aaplChart.update();
+
+  googlChart.data.datasets[0].data = googlPriceData;
+  googlChart.data.labels = Array.from({ length: numDataPoints }, (_, i) => `${dataInterval} ${i + 1}`);
+  googlChart.options.scales.x.ticks.maxTicksLimit = maxTicksLimit;
+  googlChart.update();
+}
+
 // Generate initial data for AAPL and GOOGL
-aaplPriceData = Array.from({ length: 10 }, () => generatePriceData());
-googlPriceData = Array.from({ length: 10 }, () => generatePriceData());
+aaplPriceData = Array.from({ length: 10 }, generateRandomPrices);
+googlPriceData = Array.from({ length: 10 }, generateRandomPrices);
 
 // Create initial AAPL Chart
 const aaplChart = createChart('aapl-chart', 'AAPL', aaplPriceData);
@@ -60,114 +128,121 @@ const aaplChart = createChart('aapl-chart', 'AAPL', aaplPriceData);
 // Create initial GOOGL Chart
 const googlChart = createChart('googl-chart', 'GOOGL', googlPriceData);
 
-// Function to update chart interval
-function changeChartInterval(interval) {
-  // Generate new data based on the selected interval
-  const numDays = 10;
-  const dataInterval = interval === 'daily' ? 'Day' : interval === 'hourly' ? 'Hour' : 'Minute';
-  const newData = Array.from({ length: numDays }, () => generatePriceData());
-
-  // Update the chart data
-  aaplChart.data.labels = Array.from({ length: numDays }, (_, i) => `${dataInterval} ${i + 1}`);
-  aaplChart.data.datasets[0].data = newData;
-  aaplPriceData = newData; // Update the price data array
-  aaplChart.update();
-
-  googlChart.data.labels = Array.from({ length: numDays }, (_, i) => `${dataInterval} ${i + 1}`);
-  googlChart.data.datasets[0].data = newData;
-  googlPriceData = newData; // Update the price data array
-  googlChart.update();
-}
-
+// Start updating stock prices every 10 seconds
+setInterval(updateStockPrices, 10000);
 function buyStock(stock) {
-  let priceData;
-  let stockPriceElement;
-  let accountBalanceElement;
-
-  if (stock === 'AAPL') {
-    priceData = aaplPriceData;
-    stockPriceElement = document.getElementById('aapl-price');
-    accountBalanceElement = document.getElementById('account-balance');
-  } else if (stock === 'GOOGL') {
-    priceData = googlPriceData;
-    stockPriceElement = document.getElementById('googl-price');
-    accountBalanceElement = document.getElementById('account-balance');
-  } else {
-    // Handle other stocks if needed
-    return;
+    let priceData;
+    let stockPriceElement;
+    let accountBalanceElement;
+    let sharesOwned;
+  
+    if (stock === 'AAPL') {
+      priceData = aaplPriceData;
+      stockPriceElement = document.getElementById('aapl-price');
+      accountBalanceElement = document.getElementById('account-balance');
+      sharesOwned = aaplSharesOwned;
+    } else if (stock === 'GOOGL') {
+      priceData = googlPriceData;
+      stockPriceElement = document.getElementById('googl-price');
+      accountBalanceElement = document.getElementById('account-balance');
+      sharesOwned = googlSharesOwned;
+    } else {
+      // Handle other stocks if needed
+      return;
+    }
+  
+    const quantityInput = document.getElementById(`${stock.toLowerCase()}-quantity`);
+    const quantity = parseInt(quantityInput.value);
+  
+    if (isNaN(quantity) || quantity < 1) {
+      alert('Invalid quantity');
+      return;
+    }
+  
+    const stockPrice = priceData[priceData.length - 1];
+    const totalCost = quantity * stockPrice;
+    const accountBalance = parseFloat(accountBalanceElement.textContent.replace('$', ''));
+  
+    if (isNaN(totalCost) || isNaN(accountBalance)) {
+      alert('Unable to retrieve stock price or account balance');
+      return;
+    }
+  
+    if (totalCost > accountBalance) {
+      alert('Insufficient account balance');
+      return;
+    }
+  
+    const newBalance = accountBalance - totalCost;
+    accountBalanceElement.textContent = '$' + newBalance.toFixed(2);
+    alert(`Successfully bought ${quantity} shares of ${stock} for $${totalCost.toFixed(2)}`);
+  
+    // Update shares owned
+    if (stock === 'AAPL') {
+      aaplSharesOwned += quantity;
+    } else if (stock === 'GOOGL') {
+      googlSharesOwned += quantity;
+    }
   }
-
-  const quantityInput = document.getElementById(`${stock.toLowerCase()}-quantity`);
-  const quantity = parseInt(quantityInput.value);
-  if (isNaN(quantity) || quantity < 1) {
-    alert('Invalid quantity');
-    return;
-  }
-
-  const stockPrice = priceData[priceData.length - 1]; // Retrieve the current price of the stock
-  const totalCost = quantity * stockPrice;
-  const accountBalance = parseFloat(accountBalanceElement.textContent.replace('$', ''));
-
-  if (isNaN(totalCost) || isNaN(accountBalance)) {
-    alert('Unable to retrieve stock price or account balance');
-    return;
-  }
-
-  if (totalCost > accountBalance) {
-    alert('Insufficient account balance');
-    return;
-  }
-
-  const newBalance = accountBalance - totalCost;
-  accountBalanceElement.textContent = '$' + newBalance.toFixed(2);
-  alert(`Successfully bought ${quantity} shares of ${stock} for $${totalCost.toFixed(2)}`);
-}
-
-function sellStock(stock) {
-  const quantityInput = document.getElementById(`${stock.toLowerCase()}-quantity`);
-  const quantity = parseInt(quantityInput.value);
-
-  if (isNaN(quantity) || quantity < 1) {
-    alert('Invalid quantity');
-    return;
-  }
-
-  let priceData;
-  let stockPriceElement;
-  let accountBalanceElement;
-  if (stock === 'AAPL') {
-    priceData = aaplPriceData;
-    stockPriceElement = document.getElementById('aapl-price');
-    accountBalanceElement = document.getElementById('account-balance');
-  } else if (stock === 'GOOGL') {
-    priceData = googlPriceData;
-    stockPriceElement = document.getElementById('googl-price');
-    accountBalanceElement = document.getElementById('account-balance');
-  } else {
-    // Handle other stocks if needed
-    return;
-  }
-
-  if (priceData.length === 0) {
-    alert('Unable to retrieve stock price or account balance');
-    return;
-  }
-
-  const stockPrice = priceData[priceData.length - 1];
-  const totalSale = quantity * stockPrice;
-  const accountBalance = parseFloat(accountBalanceElement.textContent.replace('$', ''));
-
-  if (isNaN(stockPrice) || isNaN(accountBalance)) {
-    alert('Unable to retrieve stock price or account balance');
-    return;
-  }
-
-  if (quantity > 0 && quantity <= quantityInput.max) {
+  
+  function sellStock(stock) {
+    const quantityInput = document.getElementById(`${stock.toLowerCase()}-quantity`);
+    const quantity = parseInt(quantityInput.value);
+    let priceData;
+    let stockPriceElement;
+    let accountBalanceElement;
+    let sharesOwned;
+  
+    if (stock === 'AAPL') {
+      priceData = aaplPriceData;
+      stockPriceElement = document.getElementById('aapl-price');
+      accountBalanceElement = document.getElementById('account-balance');
+      sharesOwned = aaplSharesOwned;
+    } else if (stock === 'GOOGL') {
+      priceData = googlPriceData;
+      stockPriceElement = document.getElementById('googl-price');
+      accountBalanceElement = document.getElementById('account-balance');
+      sharesOwned = googlSharesOwned;
+    } else {
+      // Handle other stocks if needed
+      return;
+    }
+  
+    if (priceData.length === 0) {
+      alert('Unable to retrieve stock price or account balance');
+      return;
+    }
+  
+    const stockPrice = priceData[priceData.length - 1];
+    const totalSale = quantity * stockPrice;
+    const accountBalance = parseFloat(accountBalanceElement.textContent.replace('$', ''));
+  
+    if (isNaN(stockPrice) || isNaN(accountBalance)) {
+      alert('Unable to retrieve stock price or account balance');
+      return;
+    }
+  
+    if (quantity > sharesOwned) {
+      alert(`You can sell up to ${sharesOwned} shares of ${stock}`);
+      return;
+    }
+  
     const newBalance = accountBalance + totalSale;
     accountBalanceElement.textContent = '$' + newBalance.toFixed(2);
     alert(`Successfully sold ${quantity} shares of ${stock} for $${totalSale.toFixed(2)}`);
-    quantityInput.value = '';
-  } else {
-    alert('Invalid quantity');
+  
+    // Update shares owned
+    if (stock === 'AAPL') {
+      aaplSharesOwned -= quantity;
+    } else if (stock === 'GOOGL') {
+      googlSharesOwned -= quantity;
+    }
   }
-}
+  function toggleChart(chartToShowId, chartToHideId) {
+    const chartToShow = document.getElementById(chartToShowId);
+    const chartToHide = document.getElementById(chartToHideId);
+  
+    chartToShow.style.display = 'block';
+    chartToHide.style.display = 'none';
+  }
+  
